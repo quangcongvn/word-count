a269426c1579ada8c44db895e9f5c0d8e207b5f4
[Lampart] Nguyen Quang Cong <quang_cong@lampart-vn.com>
2019-05-13 11:54:38 +0700

    md

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+   alert(1234);return;
    var converter = new showdown.Converter(),
     text      = '# hello, markdown!',
     html      = converter.makeHtml(text);
