ee252ddc4c8c8572f2b3695a66af82348e62013d
[Lampart] Nguyen Quang Cong <quang_cong@lampart-vn.com>
2019-05-13 13:19:37 +0700

    x

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-    var converter = new showdown.Converter({omitExtraWLInCodeBlocks: true});
-    html = converter.makeHtml($('body').html());
-    $('body').html(html);
-}); // end ready
+// $(document).ready(function () {
+//     var converter = new showdown.Converter({omitExtraWLInCodeBlocks: true});
+//     html = converter.makeHtml($('body').html());
+//     $('body').html(html);
+// }); // end ready
---
+++ docs/js/lib/markdown/render.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    var converter = new showdown.Converter({omitExtraWLInCodeBlocks: true});
+    html = converter.makeHtml($('body').html());
+    $('body').html(html);
+}); // end ready
