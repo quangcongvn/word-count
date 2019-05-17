3b54d452efd1c0e0b96ffa8e6ca9d8fd6ffa6b52
[Lampart] Nguyen Quang Cong <quang_cong@lampart-vn.com>
2019-05-17 12:35:49 +0700

    19

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,18 +1,24 @@
-const API_KEY_CORS="5cd9396bd195ac1a818b86b8";
-$(document).ready(function () {
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://wordcount-b6bd.restdb.io/rest/words",
-        "method": "GET",
-        "headers": {
-          "content-type": "application/json",
-          "x-apikey": API_KEY_CORS,
-          "cache-control": "no-cache"
-        }
-      }
+// const API_KEY_CORS="5cd9396bd195ac1a818b86b8";
+// $(document).ready(function () {
+//     var settings = {
+//         "async": true,
+//         "crossDomain": true,
+//         "url": "https://wordcount-b6bd.restdb.io/rest/words",
+//         "method": "GET",
+//         "headers": {
+//           "content-type": "application/json",
+//           "x-apikey": API_KEY_CORS,
+//           "cache-control": "no-cache"
+//         }
+//       }
       
-      $.ajax(settings).done(function (response) {
-        console.log(response);
-      });
+//       $.ajax(settings).done(function (response) {
+//         console.log(response);
+//       });
+// }); // end ready
+
+
+
+$(document).ready(function () {
+  
 }); // end ready
